bd470a8f73e8c1133988d709e0084e26e02f0328
danielfuxioncompras-lgtm <danielfuxioncompras@gmail.com>
2026-09-22 23:31:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 //  ÚNICO archivo que debes editar en GitHub.
 //  Pega aquí la URL de tu Apps Script (termina en /exec).
 // ─────────────────────────────────────────────────────────────
-window.API_URL = 'PEGA_AQUI_LA_URL_DE_TU_APPS_SCRIPT';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbyQOVTo80KltuuEFXZEPxprM8IVWX8lMrOpSDAvFtYo_LCvUjzV0JP4bMsbPbCgn_Qucg/exec';
